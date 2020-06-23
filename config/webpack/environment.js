cb3c7b12174ca854fe432c68b410d81da9648353
Michał Krakowiak <ja.krakowiak@gmail.com>
2020-06-23 07:41:53 +0200

    Added vuetify loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,10 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const pug = require('./loaders/pug')
+const vuetify = require('./loaders/vuetify')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('pug', pug)
 environment.loaders.prepend('vue', vue)
+environment.loaders.prepend('vuetify', vuetify)
 module.exports = environment
